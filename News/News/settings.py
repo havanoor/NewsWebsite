a74466c4a72bfada6f2bb5ea706c427f5fc07af6
havanoor <havanoor@gmail.com>
2020-04-27 16:20:30 +0530

    Added home page news content

--- News/News/settings.py
+++ News/News/settings.py
@@ -117,5 +117,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+LOGIN_URL = '/login'
 
 STATIC_URL = '/static/'
+
+
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
--- News/NewsApp/views.py
+++ News/NewsApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests,json
 
 
 #
@@ -8,4 +9,12 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello")
+    url="https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=3e4b5fd607dd40289b24ac5db977cc63"
+    url2="https://newsapi.org/v2/everything?q=corona&apiKey=3e4b5fd607dd40289b24ac5db977cc63&sort-by=popularity"
+    response1=requests.get(url)
+    response2=requests.get(url2)
+    val=response1.json()
+    val2=response2.json()
+    content={'TopHeadlines':val['articles'],'Popularity':val2['articles'][0:10]}
+
+    return render(request,"index.html",content)
