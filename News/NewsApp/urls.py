065e00589d38fdbb111c58873f687631f48118d5
havanoor <havanoor@gmail.com>
2020-04-30 01:29:53 +0530

    Ui

--- News/NewsApp/urls.py
+++ News/NewsApp/urls.py
@@ -4,12 +4,13 @@ from . import views as user_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    
+
 path('',user_views.HomeView.as_view(),name="Home"),
 path('topic/<str:topic>',user_views.TopicView.as_view(),name="Topic"),
 path('login/',user_views.loginUser,name="logiin"),
 path('login2/',auth_views.LoginView.as_view(template_name='login.html')),
 path('register/',user_views.register,name="register"),
+path('logout/', user_views.logout_view, name="logout"),
 path('test/',user_views.test,name='test'),
 
 
